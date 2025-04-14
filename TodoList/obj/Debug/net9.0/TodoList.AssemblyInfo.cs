c365d4da19ef4b785fd404e2a9d26adcd68ab349
Lê Đức Kiên <152409440+leduckien25@users.noreply.github.com>
2025-04-14 15:16:15 +0700

    update code

--- TodoList/obj/Debug/net9.0/TodoList.AssemblyInfo.cs
+++ TodoList/obj/Debug/net9.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237d1654c2f0c5ca83399eb7008da701010d8849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45a2d08de7def628fb626ba9560db31c7dbd699")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
